0b037ad48782931d12084cebfd6c7480dc1ba1b1
HarshShah08 <harsh.shah@codosphere.com>
2023-06-07 18:54:57 +0530

    footer started

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,15 +4,16 @@ export default {
   theme: {
     extend: {
       colors: {
+        primary: "#071c1f",
         secondary: "#0a9a73",
         light: "#657984",
       },
       backgroundColor: {
         background: "#f3f4f6",
       },
-      boxShadow:{
-        light: "0 1px 6px 0 rgba(32, 33, 36, .28);"
-      }
+      boxShadow: {
+        light: "0 1px 6px 0 rgba(32, 33, 36, .28);",
+      },
     },
   },
   plugins: [],
