cc8034984a315825c7ab97a201193a30f7904435
HarshShah08 <harsh.shah@codosphere.com>
2023-06-08 13:52:37 +0530

    changes done

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -16,7 +16,7 @@ export default function Login() {
           heading=" Sign In To Your Account"
           subHeading="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit aliquid, Non distinctio vel iste."
         />
-        <div className="grid grid-cols-2 gap-20">
+        <div className="grid grid-cols-2 gap-20 max-w-6xl mx-auto">
           <div className="px-10">
             <form action="#">
               {fields.map((field, index) => (
